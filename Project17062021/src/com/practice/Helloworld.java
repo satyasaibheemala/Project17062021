8471cabdc9ad5731fc45ce373d5fe9f046292a3d
prasanna <prasanna@PK007>
2021-06-17 16:33:40 +0530

    First Commit

---
+++ Project17062021/src/com/practice/Helloworld.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		sayHello();
+
+	}
+	
+	public static void sayHello() {
+		
+		System.out.println("Hello World...!");
+		
+	}
+	
+	
+	
+
+}
